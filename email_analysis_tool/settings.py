a3b69adf75ca16b8129abf0ca2dc52b9ece7eb5f
abrs7 <abrishsambong@gmail.com>
2024-10-21 00:53:51 +0300

    added cors for localhost including cors credentials - google login - to with headers

--- email_analysis_tool/settings.py
+++ email_analysis_tool/settings.py
@@ -110,7 +110,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:5173',  
