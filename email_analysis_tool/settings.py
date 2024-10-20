c4e0511558b3f22b1dd05255d6ec1836111f8918
abrs7 <abrishsambong@gmail.com>
2024-10-20 11:03:26 +0300

    added cors for localhost

--- email_analysis_tool/settings.py
+++ email_analysis_tool/settings.py
@@ -107,9 +107,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'email_analysis_tool.wsgi.application'
 # CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',  
+]
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
