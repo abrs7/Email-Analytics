0037c1083ed0706ef32819fcfc584639f39d5a11
abrs7 <abrishsambong@gmail.com>
2024-10-22 00:32:41 +0300

    correct app registry

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
-from .tasks import start_scheduler
+
 
 class SchedulerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scheduler'
 
     def ready(self):
+        from .tasks import start_scheduler
         start_scheduler()
